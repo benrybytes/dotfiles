5c614d4d4ccd7a15ae1e98989fe30d22330529d3
benrybytes <benrybytes@gmail.com>
2025-02-20 19:40:24 -0800

    henry | reconfigured bspwm and polybar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,1 @@
-
-# Setting PATH for Python 3.12
-# The original version is saved in /Users/henrymartinez/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
-
-# Setting PATH for Python 3.12
-# The original version is saved in /Users/henrymartinez/.config/fish/config.fish.pysave
-set -x PATH "/Library/Frameworks/Python.framework/Versions/3.12/bin" "$PATH"
-
-eval "$(/opt/homebrew/bin/brew shellenv)"
 fastfetch
